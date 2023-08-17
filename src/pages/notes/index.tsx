175b89ee3340d7cb30ac20f532e32d87bbaf5906
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2023-08-16 22:44:32 -0400

    Started playing with supabase

--- src/pages/notes/index.tsx
+++ src/pages/notes/index.tsx
@@ -1,30 +1,85 @@
 import React from "react";
-import Link from "next/link";
-import { getNotesResponse } from "../api/notes";
-import axios from "axios";
-import { NoteWithRelations } from "../../../types";
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { createClient } from "@supabase/supabase-js";
+import { uuid } from "../../lib/utils";
 
-export async function fetchNotes(): Promise<NoteWithRelations[]> {
-  const { data } = await axios.get<getNotesResponse>("/api/notes");
-  if (!data) throw new Error("No data");
-  if (data.error) throw new Error(data.error);
-  return data.value;
-}
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const Notes: React.FC = () => {
-  const { data: notes, error, isLoading, refetch } = useQuery("notes", fetchNotes);
+  const { data: notes, isLoading } = useQuery("notes", async () => {
+    const { data, error } = await supabase.from("notes").select();
+    if (error) throw error;
+    return data;
+  });
 
-  if (error) return <p>Error :(</p>;
+  // create a new note
+  const queryClient = useQueryClient();
+  const createNote = useMutation(
+    async (note: { id: string; content: string; created_at: Date }) => {
+      await supabase.from("notes").insert(note);
+    },
+    {
+      onMutate: (note) => {
+        const notes = queryClient.getQueryData("notes") as any[];
+        console.log(note);
+        queryClient.setQueryData("notes", [...notes, note]);
+      },
+    }
+  );
+
+  const updateNote = useMutation(
+    async ({ id, content }: { id: string; content: string }) => {
+      await supabase.from("notes").update({ content }).match({ id });
+    },
+    {
+      onMutate: (note) => {
+        const notes = queryClient.getQueryData("notes") as any[];
+        const updatedNotes = notes.map((n) => {
+          if (n.id === note.id) {
+            return note;
+          }
+          return n;
+        });
+        queryClient.setQueryData("notes", updatedNotes);
+      },
+    }
+  );
+
   if (isLoading) return <p>Loading...</p>;
-
   return (
     <div>
-      {notes.map((note) => (
-        <div key={note.id}>
-          <Link href={`/notes/${note.id}`}>{note.text}</Link>
-        </div>
-      ))}
+      <div>
+        {notes
+          .sort((a, b) => (a.created_at > b.created_at ? 1 : -1))
+          .map((note) => (
+            <div key={note.id}>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  const content = (e.target as any).content.value;
+                  updateNote.mutate({ id: note.id, content });
+                }}
+              >
+                <input type="text" defaultValue={note.content} id="content" />
+              </form>
+            </div>
+          ))}
+      </div>
+      <div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const content = (e.target as any).content.value;
+            createNote.mutate({ id: uuid(), content, created_at: new Date() });
+            e.currentTarget.reset();
+          }}
+        >
+          <input type="text" name="content" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
