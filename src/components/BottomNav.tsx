a3a35aabd6d2581c693876ec271f98755b1e933e
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2023-08-28 08:30:13 -0400

    remove supabase

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,26 +1,8 @@
 import Link from "next/link";
-import { useQueryClient, useMutation } from "react-query";
-import { supabase } from "../lib/supabase";
 import { useRouter } from "next/router";
-import { uuid } from "../lib/utils";
 
 const BottomNav = () => {
-  const queryClient = useQueryClient();
-
   const router = useRouter();
-  // create a new note
-  const createTag = useMutation(
-    async (tag: { id: string; name: string }) => {
-      await supabase.from("tags").insert(tag);
-      return tag;
-    },
-    {
-      onMutate: (tag) => {
-        const tags = (queryClient.getQueryData("tags") as any[]) ?? [];
-        queryClient.setQueryData("tags", [...tags, tag]);
-      },
-    }
-  );
 
   return (
     <>
