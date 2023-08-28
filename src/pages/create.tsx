d9d719a49f347469d3df3e474ee327c8a74cccfb
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2023-08-28 07:24:29 -0400

    wip

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useMutation, useQuery, useQueryClient } from "react-query";
-import { generateKeyBetween } from "fractional-indexing";
+import { generateKeyBetween, generateNKeysBetween } from "fractional-indexing";
 import { useRouter } from "next/router";
 import { uuid } from "../lib/utils";
 import { useRef, useState } from "react";
@@ -10,20 +10,48 @@ type Tag = {
   name: string;
 };
 
-type TagWithLastPosition = Tag & { lastPosition: string };
+type TagWithLastPosition = Tag & { prev_last_position: string };
 
 type Note = {
   id: string;
   content: string;
   created_at: string;
-  position: string; // position in the hidden tag
-  otherTagPositions: { tagId: string; position: string }[];
+  positions: { [tagId: string]: string };
 };
+
+// const prevLastPositions = [
+//   { tagId: "64b9c132-006c-4302-a3d1-f7254429808a", position: "a0" },
+//   { tagId: "a0b9c132-006c-4302-a3d1-f7254429808a", position: "a1" },
+//   { tagId: "b0b9c132-006c-4302-a3d1-f7254429808a", position: null },
+// ];
+
+// const notes = [
+//   {
+//     id: "abcde",
+//     content: "note 1",
+//     created_at: "2021-01-01",
+//     position: [
+//       { tagId: "64b9c132-006c-4302-a3d1-f7254429808a", position: "a0" },
+//       { tagId: "a0b9c132-006c-4302-a3d1-f7254429808a", position: "a3" },
+//     ],
+//   },
+//   {
+//     id: "fghij",
+//     content: "note 2",
+//     created_at: "2021-01-02",
+//     position: [
+//       { tagId: "64b9c132-006c-4302-a3d1-f7254429808a", position: "a1" },
+//       { tagId: "a0b9c132-006c-4302-a3d1-f7254429808a", position: "a2" },
+//     ],
+//   },
+// ];
 
 // export default function Create({ tagId = "64b9c132-006c-4302-a3d1-f7254429808a" }: { tagId: string }) {
 export default function Create() {
-  const hiddenTag = "64b9c132-006c-4302-a3d1-f7254429808a";
-  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const [selectedTags, setSelectedTags] = useState<string[]>([
+    "64b9c132-006c-4302-a3d1-f7254429808a",
+  ]);
+  console.log(selectedTags);
   const inputRef = useRef<HTMLInputElement>(null);
   const [newTag, setNewTag] = useState("");
   const queryClient = useQueryClient();
@@ -34,23 +62,23 @@ export default function Create() {
     async () => {
       const result = await axios.post("/api/db", [
         {
-          query: `select tag.*, max(tag_entries.position) last_position from tag left join tag_entries on tag.id = tag_entries.tag_id group by tag.id`,
+          query: `select tag.*, max(tag_entries.position) prev_last_position from tag left join tag_entries on tag.id = tag_entries.tag_id group by tag.id`,
         },
       ]);
-      return result.data.reduce((acc, { id, name, last_position }) => {
-        acc[id] = { id, name, lastPosition: last_position ?? null };
+      return result.data.reduce((acc, { id, name, prev_last_position }) => {
+        acc[id] = { id, name, prev_last_position: prev_last_position ?? null };
         return acc;
       }, {});
     }
   );
 
   const { data: notes, isLoading: notesLoading } = useQuery<Note[], any>(
-    ["note", hiddenTag],
+    ["note", selectedTags[0]],
     async () => {
       const result = await axios.post("/api/db", [
         {
           query: `select note.*, tag_entries.position from note join tag_entries on note.id = tag_entries.note_id where tag_entries.tag_id = $1`,
-          params: [hiddenTag],
+          params: [selectedTags[0]],
         },
       ]);
       return result.data.map((row) => {
@@ -58,8 +86,7 @@ export default function Create() {
           id: row.id,
           content: row.content,
           created_at: row.created_at,
-          position: row.position,
-          otherTagPositions: [],
+          positions: { [selectedTags[0]]: row.position },
         };
         return note;
       });
@@ -67,75 +94,105 @@ export default function Create() {
   );
 
   const { mutate: createNote } = useMutation(
-    async (note: {
-      id: string;
-      content: string;
-      position: string;
-      created_at: string;
-      otherTagPositions: { tagId: string; position: string }[];
-    }) => {
+    async (note: Note) => {
+      console.log("new note:", note);
       await axios.post("/api/db", [
         {
           query: `insert into note (id, content, created_at) values ($1, $2, $3)`,
           params: [note.id, note.content, note.created_at],
         },
-        [...note.otherTagPositions, { tagId: hiddenTag, position: note.position }].map(
-          ({ tagId, position }) => ({
-            query: `insert into tag_entries (tag_id, note_id, position) values ($1, $2, $3)`,
-            params: [tagId, note.id, position],
-          })
-        ),
+        ...Object.entries(note.positions).map(([tagId, position]) => ({
+          query: `insert into tag_entries (tag_id, note_id, position) values ($1, $2, $3)`,
+          params: [tagId, note.id, position],
+        })),
       ]);
     },
     {
       onMutate: (note) => {
-        const notes = queryClient.getQueryData(["note", hiddenTag]) as any[];
-        queryClient.setQueryData(["note", hiddenTag], [...notes, note]);
-      },
-      onError: () => {
-        queryClient.invalidateQueries(["note", hiddenTag]);
+        const notes = queryClient.getQueryData(["note", selectedTags[0]]) as any[];
+        queryClient.setQueryData(["note", selectedTags[0]], [...notes, note]);
       },
     }
   );
 
+  const { mutate: updateNote } = useMutation(
+    async ({ id, content }: { id: string; content: string }) => {
+      await axios.post("/api/db", [
+        {
+          query: `update note set content = $1 where id = $2`,
+          params: [content, id],
+        },
+      ]);
+    },
+    {
+      onMutate: (note) => {
+        const notes = queryClient.getQueryData(["note", selectedTags[0]]) as any[];
+        const updatedNotes = notes.map((n) => {
+          if (n.id === note.id) {
+            return { ...n, ...note };
+          }
+          return n;
+        });
+        queryClient.setQueryData(["note", selectedTags[0]], updatedNotes);
+      },
+    }
+  );
+
+  const { mutate: deleteNote } = useMutation(
+    async (id: string) => {
+      await axios.post("/api/db", [
+        {
+          query: `delete from note where id = $1`,
+          params: [id],
+        },
+      ]);
+    },
+    {
+      onMutate: (id) => {
+        const notes = queryClient.getQueryData(["note", selectedTags[0]]) as any[];
+        const updatedNotes = notes.filter((n) => n.id !== id);
+        queryClient.setQueryData(["note", selectedTags[0]], updatedNotes);
+      },
+    }
+  );
+
+  const { mutate: addTag } = useMutation(
+    async (tagId: string) => {
+        await axios.post("/api/db", [
+            {
+            query: `insert into tag_entries (tag_id, note_id, position) values ($1, $2, $3)`,
+            params: [tagId, notes[0].id, generateKeyBetween(null, null)],
+            },
+        ]);
+    }
+
   if (!allTags || notesLoading) return null;
 
   const sortedNotes = notes.sort((a, b) => {
-    if (a.position === b.position) {
+    const aPos = Object.values(a.positions)[0];
+    const bPos = Object.values(b.positions)[0];
+    if (aPos === bPos) {
       return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
     }
-    return a.position > b.position ? 1 : -1;
+    return aPos > bPos ? 1 : -1;
   });
-  console.log(sortedNotes);
 
-  // lastPosition in each of the selected tags. If sortedNotes is empty, it will be the lastPosition
-  // in the allTags object. Otherwise, it will be the lastPosition in the sortedNotes array.
-  let lastPositions: { [key: string]: string } = {};
-  const lastNote = sortedNotes.slice(-1)[0];
-  if (!lastNote) {
-    lastPositions = selectedTags.reduce((acc, tagId) => {
-      acc[tagId] = allTags[tagId].lastPosition;
-      return acc;
-    }, {});
-    lastPositions[hiddenTag] = allTags[hiddenTag].lastPosition;
-  } else {
-    lastPositions = lastNote.otherTagPositions.reduce((acc, { tagId, position }) => {
-      acc[tagId] = position;
-      return acc;
-    }, {});
-    lastPositions[hiddenTag] = lastNote.position;
-  }
-  console.log(lastPositions);
+  // If notes have been added, the last position is the last note's position.
+  // Otherwise, it's the tag's last position at load time.
+  const lastPositions =
+    sortedNotes.slice(-1)[0]?.positions ??
+    Object.fromEntries(selectedTags.map((tagId) => [tagId, allTags[tagId].prev_last_position]));
 
+  console.log({ sortedNotes, lastPositions });
   //   const lastPosition = sortedNotes.slice(-1)[0]?.position ?? null;
 
   return (
     <div className="max-w-2xl mx-auto flex flex-col items-center">
       <div>
         <h1>Tags</h1>
-        <span>Hidden tag: {hiddenTag}</span>
         <div className="flex flex-row flex-wrap">
           {selectedTags
+            .slice(1)
             .map((id) => allTags[id])
             .map((tag) => (
               <span className="bg-blue-200 rounded px-2 py-1 m-1 flex items-center" key={tag.id}>
@@ -203,17 +260,17 @@ export default function Create() {
                 className="p-4"
                 contentEditable
                 suppressContentEditableWarning
-                // onBlur={(e) => {
-                //   const content = e.currentTarget.textContent;
-                //   updateNote({ id: note.id, content });
-                // }}
+                onBlur={(e) => {
+                  const content = e.currentTarget.textContent;
+                  updateNote({ id: note.id, content });
+                }}
                 // // delete on backspace if empty
-                // onKeyDown={(e) => {
-                //   if (e.key === "Backspace" && e.currentTarget.textContent === "") {
-                //     e.preventDefault();
-                //     deleteNote(note.id);
-                //   }
-                // }}
+                onKeyDown={(e) => {
+                  if (e.key === "Backspace" && e.currentTarget.textContent === "") {
+                    e.preventDefault();
+                    deleteNote(note.id);
+                  }
+                }}
                 dangerouslySetInnerHTML={{ __html: note.content }}
               />
             </div>
@@ -223,16 +280,13 @@ export default function Create() {
       <button
         className="w-8 border border-black rounded-md my-4"
         onClick={() =>
-          //   createNote({ id: uuid(), content: "", position: generateKeyBetween(lastPosition, null) })
           createNote({
             id: uuid(),
             content: "",
             created_at: new Date().toISOString(),
-            position: generateKeyBetween(lastPositions[hiddenTag], null),
-            otherTagPositions: selectedTags.map((tagId) => ({
-              tagId,
-              position: generateKeyBetween(lastPositions[tagId], null),
-            })),
+            positions: Object.fromEntries(
+              selectedTags.map((tagId) => [tagId, generateKeyBetween(lastPositions[tagId], null)])
+            ),
           })
         }
       >
